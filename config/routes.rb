bbfa3852d6bc62b99133b6eb34055ed90c287a29
Farooq Ezhar <waghih.co@gmail.com>
2021-12-09 16:05:15 +0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    scope :posts, controller: :posts do
+      get :top
+    end
+
+    scope :comments, controller: :comments do
+      get :search
+    end
+  end
 end
